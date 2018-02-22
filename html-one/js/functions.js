e569cddb057e3fac0e3774e1ab2bd4b2c468a417
Gabriel Chacan <36725378+GabrielCh97@users.noreply.github.com>
2018-02-22 02:49:52 -0500

    Add files via upload

---
+++ html-one/js/functions.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+setInterval(()=>{
+  $('#add-img').attr({'src': 'img/logo.png'}).fadeTo(800,1);
+  $('#add-img').attr({'src': 'img/logo.png'}).fadeTo(800,0);
+}, 1500)
+
+
+});
